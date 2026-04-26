1a2e1822a29f928858f40124d2e2a26585b7f2e6
stephany <stephany.cornejo00@gmail.com>
2026-04-25 19:40:48 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ColegioSanJose.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
