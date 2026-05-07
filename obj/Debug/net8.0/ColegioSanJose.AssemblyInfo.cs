c1fe53b1cff938c049dc6828d22dc3377f10178e
stephany <stephany.cornejo00@gmail.com>
2026-05-06 19:09:21 -0600

    agregue un modal para confirmar eliminacion de expediente

--- obj/Debug/net8.0/ColegioSanJose.AssemblyInfo.cs
+++ obj/Debug/net8.0/ColegioSanJose.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColegioSanJose")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25226b4c935a0a1156e0ec052ed049989edc4abe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9db2b1de7c7a52944e4a7b2e9f36c9e98aa3f7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColegioSanJose")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColegioSanJose")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
